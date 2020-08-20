3560b20d209367ecec6c83179f203e00a6aa7ad1
Agay Borges <agay.borges@hivecloud.com.br>
2020-08-20 11:09:05 -0300

    move note-api to root

--- delete.js
+++ delete.js
@@ -1,10 +1,10 @@
-import handler from "../libs/handler-lib";
-import dynamoDb from "../libs/dynamodb-lib";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // 'Key' defines the partition key and sort key of the item to be removed
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
@@ -13,11 +13,7 @@ export const main = handler(async (event, context) => {
     }
   };
 
-  const result = await dynamoDb.get(params);
-  if ( ! result.Item) {
-    throw new Error("Item not found.");
-  }
+  await dynamoDb.delete(params);
 
-  // Return the retrieved item
-  return result.Item;
+  return { status: true };
 });
--- get.js
+++ get.js
@@ -1,10 +1,10 @@
-import handler from "../libs/handler-lib";
-import dynamoDb from "../libs/dynamodb-lib";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be removed
+    // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
@@ -13,7 +13,11 @@ export const main = handler(async (event, context) => {
     }
   };
 
-  await dynamoDb.delete(params);
+  const result = await dynamoDb.get(params);
+  if ( ! result.Item) {
+    throw new Error("Item not found.");
+  }
 
-  return { status: true };
+  // Return the retrieved item
+  return result.Item;
 });
